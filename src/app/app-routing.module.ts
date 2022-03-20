47e39f5e326b3011ee3aa90ab196e3e92774d448
smoonmare <smoonmare@gmail.com>
2022-03-19 20:11:17 -0700

    Configured routes. Added router outlet. Configured routerLinks.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'users', component: UsersComponent},
+  { path: 'servers', component: ServersComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
