b739eb47e6a43699109883d67a3866d15379f48c
smoonmare <smoonmare@gmail.com>
2022-03-20 20:52:10 -0700

    Added Route Observable to update 'id' and 'name' fields.

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -8,6 +9,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UserComponent implements OnInit {
   user?: {id: number, name: string};
+  paramsSubscription?: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -15,7 +17,16 @@ export class UserComponent implements OnInit {
     this.user = {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
-    }
+    };
+    this.paramsSubscription = this.route.params
+      .subscribe((params: Params) => {
+        this.user!.id = params['id'];
+        this.user!.name = params['name'];
+      });
+  }
+
+  ngOnDestroy() {
+    this.paramsSubscription?.unsubscribe();
   }
 
 }
