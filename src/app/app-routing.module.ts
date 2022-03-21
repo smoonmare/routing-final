52647bd9ba4e7a9970331983f126483717082d9c
smoonmare <smoonmare@gmail.com>
2022-03-20 21:55:31 -0700

    Added Route to ServerComponent. Used Router params to retrieve server info.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
 import { UsersComponent } from './users/users.component';
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'users', component: UsersComponent},
   { path: 'users/:id/:name', component: UserComponent},
   { path: 'servers', component: ServersComponent},
+  { path: 'server/:id', component: ServerComponent},
   { path: 'servers/:id/edit', component: EditServerComponent}
 ];
 
