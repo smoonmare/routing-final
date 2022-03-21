3f812cdda72bf92bd8953abff86e359b9c13ff92
smoonmare <smoonmare@gmail.com>
2022-03-20 22:01:25 -0700

    Added children to servers route. Optimized code. Moved Reload Page button from router outlet div.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
@@ -11,9 +12,10 @@ const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'users', component: UsersComponent},
   { path: 'users/:id/:name', component: UserComponent},
-  { path: 'servers', component: ServersComponent},
-  { path: 'server/:id', component: ServerComponent},
-  { path: 'servers/:id/edit', component: EditServerComponent}
+  { path: 'servers', component: ServersComponent, children: [
+    { path: ':id', component: ServerComponent},
+    { path: ':id/edit', component: EditServerComponent}
+  ]}
 ];
 
 @NgModule({
